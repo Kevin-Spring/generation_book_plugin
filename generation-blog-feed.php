be5c0aafa8cd19bc4099aa14ec9490d9278f4ec1
Kevin Spring <kevinspring93@hotmail.com>
2020-12-07 16:59:51 +0100

    added custom meta boxes

--- generation-blog-feed.php
+++ generation-blog-feed.php
@@ -21,30 +21,30 @@ add_action( 'wp_enqueue_scripts', 'my_css' );
 function init_generation_books_post_type() {
 
     $labels = [
-        'name'                  => _x( 'Books', 'Post type general name', 'textdomain' ),
-        'singular_name'         => _x( 'Book', 'Post type singular name', 'textdomain' ),
-        'menu_name'             => _x( 'Books', 'Admin Menu text', 'textdomain' ),
-        'name_admin_bar'        => _x( 'Book', 'Add New on Toolbar', 'textdomain' ),
-        'add_new'               => __( 'Add New', 'textdomain' ),
-        'add_new_item'          => __( 'Add New Book', 'textdomain' ),
-        'new_item'              => __( 'New Book', 'textdomain' ),
-        'edit_item'             => __( 'Edit Book', 'textdomain' ),
-        'view_item'             => __( 'View Book', 'textdomain' ),
-        'all_items'             => __( 'All Books', 'textdomain' ),
-        'search_items'          => __( 'Search Books', 'textdomain' ),
-        'parent_item_colon'     => __( 'Parent Books:', 'textdomain' ),
-        'not_found'             => __( 'No books found.', 'textdomain' ),
-        'not_found_in_trash'    => __( 'No books found in Trash.', 'textdomain' ),
-        'featured_image'        => _x( 'Book Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
-        'archives'              => _x( 'Book archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
-        'insert_into_item'      => _x( 'Insert into book', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
-        'uploaded_to_this_item' => _x( 'Uploaded to this book', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
-        'filter_items_list'     => _x( 'Filter books list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
-        'items_list_navigation' => _x( 'Books list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
-        'items_list'            => _x( 'Books list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
+        'name'                  => __( 'Books', 'Post type general name'),
+        'singular_name'         => __( 'Book', 'Post type singular name'),
+        'menu_name'             => __( 'Books', 'Admin Menu text'),
+        'name_admin_bar'        => __( 'Book', 'Add New on Toolbar'),
+        'add_new'               => __( 'Add New'),
+        'add_new_item'          => __( 'Add New Book'),
+        'new_item'              => __( 'New Book'),
+        'edit_item'             => __( 'Edit Book'),
+        'view_item'             => __( 'View Book'),
+        'all_items'             => __( 'All Books'),
+        'search_items'          => __( 'Search Books'),
+        'parent_item_colon'     => __( 'Parent Books:'),
+        'not_found'             => __( 'No books found.'),
+        'not_found_in_trash'    => __( 'No books found in Trash.'),
+        'featured_image'        => __( 'Book Cover Image', 'Overrides the “Featured Image” phrase for this post type.'),
+        'set_featured_image'    => __( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type.'),
+        'remove_featured_image' => __( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type.'),
+        'use_featured_image'    => __( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type.'),
+        'archives'              => __( 'Book archives', 'The post type archive label used in nav menus. Default “Post Archives”.'),
+        'insert_into_item'      => __( 'Insert into book', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post).'),
+        'uploaded_to_this_item' => __( 'Uploaded to this book', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post).'),
+        'filter_items_list'     => __( 'Filter books list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”.'),
+        'items_list_navigation' => __( 'Books list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”.'),
+        'items_list'            => __( 'Books list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”.'),
     ];
 
     $args = [
@@ -71,12 +71,14 @@ function init_generation_books_post_type() {
 add_action( 'init', 'init_generation_books_post_type' );
 
 
-/* Metabox */
+/**  
+ * Metabox 
+ */
 function add_generation_books_metaboxes() {
     
     add_meta_box(
         'generation_books_metaboxes',
-        'Book Options',
+        'Book Info',
         'generation_books_metaboxes',
         'books',
         'side',
@@ -84,6 +86,9 @@ function add_generation_books_metaboxes() {
     );
 }
 
+/**
+ * Output the HTML for the metabox.
+ */
 function generation_books_metaboxes(){
 
     global $post;
@@ -94,24 +99,80 @@ function generation_books_metaboxes(){
 	// Get the location data if it's already been entered
     $price = get_post_meta( $post->ID, 'price', true );
     $author = get_post_meta( $post->ID, 'author', true );
+    $genre = get_post_meta( $post->ID, 'genre', true );
 
     // Output the field
     ?>
-    <label for="price">Pris</label> 
-    <input type="number" name="price" value=" <?php esc_textarea( $price ) ?>" class="widefat">
-    <label for="genre">Genre</label>
-    <select name="genre" id="genre" class="postbox">
-        <option value="">Select something...</option>
-        <option value="barn" <?php selected( $value, 'barn' ); ?>>Barn</option>
-        <option value="thriller" <?php /* selected( $value, 'thriller' ); */ ?>>Thriller</option>
-        <option value="äventyr" <?php /* selected( $value, 'äventyr' ); */ ?>>Äventyr</option>
-    </select>
+    
     <label for="author">Författare</label>
-    <input type="text" name="author" value=" <?php esc_textarea( $author ) ?>" class="widefat">
+    <input type="text" name="author" value="<?php echo $author ?>" class="widefat">
+
+    <label for="genre">Genre</label>
+    <select name="genre" id="genre" class="widefat">
+
+        <option value="<?php echo $genre ?>"> <?php echo $genre ?> </option>
+
+        <option value="barn" <?php selected( 'genre', 'barn' ); ?>>Barn</option>
+        <option value="thriller" <?php selected( 'genre', 'thriller' ); ?>>Thriller</option>
+        <option value="äventyr" <?php selected( 'genre', 'äventyr' ); ?>>Äventyr</option>
+
+    </select>
+    
+    <label for="price">Pris</label> 
+    <input type="number" name="price" value="<?php echo $price ?>" class="widefat">
+    
 
     <?php 
 
 }
+
+/**
+ * Save the metabox data
+ */
+function generation_books_save_events_meta( $post_id, $post ) {
+
+	// Return if the user doesn't have edit permissions.
+	if ( ! current_user_can( 'edit_post', $post_id ) ) {
+        return $post_id;
+	}
+
+	// Verify this came from the our screen and with proper authorization,
+    // because save_post can be triggered at other times.
+    if ( ! isset( $_POST['author'] )  || ! isset( $_POST['price'] ) || ! isset( $_POST['genre'] )  || ! wp_verify_nonce( $_POST['books_fields'], basename(__FILE__) ) ) {
+		return $post_id;
+	}
+
+	// Now that we're authenticated, time to save the data.
+	// This sanitizes the data from the field and saves it into an array $events_meta.
+    $books_meta['author'] = esc_textarea( $_POST['author'] );
+    $books_meta['price'] = $_POST['price'];
+    $books_meta['genre'] = $_POST['genre'];
+
+	// Cycle through the $books_meta array.
+	foreach ( $books_meta as $key => $value ) :
+
+		// Don't store custom data twice
+		if ( 'revision' === $post->post_type ) {
+			return;
+		}
+
+		if ( get_post_meta( $post_id, $key, false ) ) {
+			// If the custom field already has a value, update it.
+			update_post_meta( $post_id, $key, $value );
+		} else {
+			// If the custom field doesn't have a value, add it.
+			add_post_meta( $post_id, $key, $value);
+		}
+
+		if ( ! $value ) {
+			// Delete the meta key if there's no value
+			delete_post_meta( $post_id, $key );
+		}
+
+	endforeach;
+
+}
+add_action( 'save_post', 'generation_books_save_events_meta', 1, 2 );
 
 
 
